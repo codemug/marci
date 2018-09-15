ef3411c248260d727118ddb31878e0751f678825
Usman Shahid <usman.shahid@intechww.com>
2018-09-15 17:33:42 +0500

    Added readme and documentation

---
+++ README.md
@@ -0,0 +1,106 @@
+# Marci
+
+Navigate your python process' memory. Analyze objects and their relations to other objects.
+
+### Motivation
+
+During a memory leak detection spree, I came across multiple tools but I couldn't get them to analyze, navigate and traverse object graphs in memory like I wanted. My requirements forced me to use the `gc` package directly and I ended up writing snippets of memory analysis code that I needed to run again and again so I ended up packing these snippets in this package.
+
+
+### Installation
+
+To install, simply clone the source, navigate into the cloned directory and do a:
+```
+pip install .
+```
+I might be publishing a PyPi package pretty soon
+
+### Usage
+
+Fire up a python interpreter in your favorite terminal and run the following code:
+
+```
+from marci import ObjList, Capture
+c = Capture()
+c.start()
+
+# Create some data, or do whatever needs to be observed for memory growth
+some_data = {
+    "string": "cobra"
+}
+more_data = [some_data]
+obj_list = c.end()
+```
+Now obj_list contains all the objects that were created between the start() and the end() method It's a list with additional methods, so you can use it as a normal list:
+```
+>>> len(obj_list)
+7
+```
+
+Additional methods include pretty printing
+```
+>>> obj_list.pprint()
+(0, {})
+(1, [{'string': 'cobra'}])
+(2, ['obj_list', 'c', 'end'])
+(3, ('c', 'end', 'obj_list'))
+(4, (None,))
+(5, <frame object at 0x7fdbaf32e548>)
+(6, <frame object at 0x7fdbaf306810>)
+```
+
+Type to object count summary printing
+
+```
+>>> obj_list.psummary()
+ <type 'dict'>	1
+ <type 'list'>	2
+<type 'frame'>	2
+<type 'tuple'>	2
+```
+
+Extracting a specific type of objects from this list
+
+```
+>>> l_list = obj_list.extract(list)
+>>> l_list.pprint()
+(0, [{'string': 'cobra'}])
+(1, ['obj_list', 'c', 'end'])
+```
+Or multiple types
+
+```
+>>> l_list = obj_list.extract([list,dict])
+>>> l_list.pprint()
+(0, {})
+(1, [{'string': 'cobra'}])
+(2, ['obj_list', 'c', 'end'])
+```
+
+Exclude specific type(s) of objects from the list
+```
+>>> l_list = obj_list.sanitize(dict)
+>>> l_list.pprint()
+(0, [{'string': 'cobra'}])
+(1, ['obj_list', 'c', 'end'])
+(2, ('c', 'end', 'obj_list'))
+(3, (None,))
+(4, <frame object at 0x7fdbaf32e548>)
+(5, <frame object at 0x7fdbaf306810>)
+```
+Get a list of objects that are referencing an object inside the list
+
+```
+ >>> refs = l_list.refs_to_at(0)
+ >>> refs.pprint(100)
+ (0, '<frame object at 0x7fdbaf2e1420>')
+ (1, '<frame object at 0x7fdbaf2e1608>')
+```
+Or a list of objects that are being referenced by an object inside the list
+```
+>>> refs = l_list.refs_by_at(0)
+>>> refs.pprint()
+(0, {'string': 'cobra'})
+```
+
+
--- marci/tools.py
+++ marci/tools.py
@@ -2,14 +2,44 @@ import gc
 
 
 class Capture:
+  """
+  A ``Capture`` object is used to capture all the objects created during the
+  execution of the code that is between it's ``start`` and ``end`` methods as
+  an ``ObjList``. The ``Capture`` object is recyclable in the sense that each
+  new ``start`` creates a new checkpoint, discarding the last one. The
+  constructor also takes a boolean ``start`` argument which, if set to true,
+  also creates the first checkpoint.
+
+  Example::
+
+    from marci import Capture
+    capture = Capture()
+    capture.start()
+    myObject.MyMethod()
+    obj_list = capture.end()
+
+  """
+
   def __init__(self, start=False):
+    """
+    Initialize a new ``Capture`` instance
+    :param start: Also create the first checkpoint
+    """
     if start is True:
       self.start()
 
   def start(self):
+    """
+     Creates a new checkpoint of the memory
+    """
     self.begin = gc.get_objects()
 
   def end(self):
+    """
+    Ends the capture, finds out the new objects created between the ``start``
+    call and the moment ``end`` is called.
+    :return: An ``ObjList`` of newly created objects
+    """
     end = gc.get_objects()
     obj_list = ObjList(empty=True)
     for item in end:
@@ -28,7 +58,17 @@ class Capture:
 
 
 class ObjList(list):
+  """
+  A ``list()`` with additional methods for memory analysis and object graph
+  traversal
+  """
+
   def __init__(self, empty=False):
+    """
+    Initialize a new ``ObjList``
+    :param empty: Create an empty ``ObjList``, otherwise fill it with current
+      memory objects
+    """
     if not empty:
       for item in gc.get_objects():
         t = type(item)
@@ -36,6 +76,12 @@ class ObjList(list):
           self.append(item)
 
   def __sub__(self, other):
+    """
+    Subtract another ``ObjList`` from this one. This method doesn't mutate the
+    original ``ObjList``s
+    :param other: The other ``ObjList``
+    :return: An `ObjList` containing the subtraction result
+    """
     if type(other) is not ObjList:
       raise TypeError(
           "Cannot subtract " + str(type(other)) + ' from an ObjList')
@@ -51,6 +97,13 @@ class ObjList(list):
     return result
 
   def extract(self, ex_type):
+    """
+    Extract specific type(s) of objects from this ``ObjList``. This does not
+    mutate the existing ``ObjList``
+    :param ex_type: A ``type()`` or a ``list()`` of ``type()`` that should be
+      included
+    :return: ``ObjList`` that only contains the objects of the specified type(s)
+    """
     new_list = ObjList(empty=True)
     if type(ex_type) is list:
       for item in self:
@@ -63,6 +116,14 @@ class ObjList(list):
     return new_list
 
   def sanitize(self, ex_type):
+    """
+    Exclude specific type(s) of object from this ``ObjList``. This does not
+    mutate the existing ``ObjList``
+    :param ex_type: ex_type: A ``type()`` or a ``list()`` of ``type()``
+      that should be included
+    :return: ``ObjList`` that does not contain the objects of the
+      specified type(s)
+    """
     new_list = ObjList(empty=True)
     if type(ex_type) is list:
       for item in self:
@@ -75,11 +136,22 @@ class ObjList(list):
     return new_list
 
   def refs_to_at(self, pos):
+    """
+    Get the objects that hold a reference to the object as a specified position
+    in this ``ObjList``
+    :param pos: Index position of the object that is being referenced
+    :return: ``ObjList`` containing the referrer objects
+    """
     if len(self) >= pos:
       return self.refs_to(self[pos])
     return ObjList(empty=True)
 
   def refs_to(self, obj):
+    """
+    Get the objects that hold a reference to the specified object
+    :param obj: The object that is being referenced
+    :return: ``ObjList`` containing the referrer objects
+    """
     new_list = ObjList(empty=True)
     refs = gc.get_referrers(obj)
     for item in refs:
@@ -89,11 +161,22 @@ class ObjList(list):
     return new_list
 
   def refs_by_at(self, pos):
+    """
+    Get the objects that are referred by the object at a specified position in
+    this ``ObjList``
+    :param pos: Index position of the object that is referencing
+    :return: ``ObjList`` containing the referee objects
+    """
     if len(self) >= pos:
       return self.refs_by(self[pos])
     return ObjList(empty=True)
 
   def refs_by(self, obj):
+    """
+    Get the objects that are referred by the  specified object
+    :param obj: The object that is referring
+    :return: ``ObjList`` containing the referee objects
+    """
     new_list = ObjList(empty=True)
     refs = gc.get_referents(obj)
     for item in refs:
@@ -103,6 +186,10 @@ class ObjList(list):
     return new_list
 
   def pprint(self, obj_len=None):
+    """
+    Pretty print the list with index values
+    :param obj_len: Maximum length of the string that represents the objects
+    """
     if obj_len is not None and type(obj_len) is int and obj_len > 0:
       f_str = '{:.' + str(obj_len) + '}'
       for i in range(0, len(self)):
@@ -112,6 +199,10 @@ class ObjList(list):
         print(i, self[i])
 
   def pprintt(self, obj_len=None):
+    """
+    Pretty print the list with index values and object types
+    :param obj_len: Maximum length of the string that represents the objects
+    """
     if obj_len is not None and type(obj_len) is int and obj_len > 0:
       f_str = '{:.' + str(obj_len) + '}'
       for i in range(0, len(self)):
@@ -121,6 +212,10 @@ class ObjList(list):
         print(i, type(self[i]), self[i])
 
   def psummary(self):
+    """
+    Print a summary of all the objects. Prints a table containing counts for
+     every object type initialized
+    """
     histo = dict()
     llen = 0
     for item in self:
